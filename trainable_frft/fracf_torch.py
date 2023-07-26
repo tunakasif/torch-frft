fix: incorrect calculation of `corefrmod2()` (#2)
Multiplication with the indices was present instead of a multiplication
with the input signal, this incorrect behavior has been fixed.

A final `deltax` division was forgotten in the final step of the result
calculation.

Add additional fractional order (a=0.5) test for the helper
the function of `corefrmod2`.

Fixes: #1

trainable_frft/fracf_torch.py
@@ -91,7 +91,7 @@ def corefrmod2(signal: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
     # Chirp Multiplication
     x = torch.arange(Nstart, Nend) / deltax
     chirp = torch.exp(alpha * x**2)
-    multip = torch.mul(x, chirp)
+    multip = torch.mul(signal, chirp)
 
     # Chirp Convolution
     t = torch.arange(-N + 1, N) / deltax
@@ -108,7 +108,7 @@ def corefrmod2(signal: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
     Hc = Hc[N - 1 : 2 * N - 1]
 
     # Chirp Multiplication
-    result = torch.mul(Aphi * chirp, Hc)
+    result = torch.mul(Aphi * chirp, Hc) / deltax
 
     # Adjustment
     if N % 2 == 1: