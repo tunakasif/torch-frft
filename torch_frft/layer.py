6e164a8edfaf53b2927cbb3cabddfb6f7e1434f6
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-03 01:28:39 +0300

    fix(layer): add dimension to `FrFT`
    Add dimension selection to the FrFT layer.

--- torch_frft/layer.py
+++ torch_frft/layer.py
@@ -5,9 +5,10 @@ from torch_frft.frft import frft
 
 
 class FrFTLayer(nn.Module):
-    def __init__(self, order: float = 1.0) -> None:
+    def __init__(self, order: float = 1.0, *, dim: int = -1) -> None:
         super().__init__()
         self.order = nn.Parameter(torch.tensor(order, dtype=torch.float32))
+        self.dim = dim
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return frft(x, self.order)
+        return frft(x, self.order, dim=self.dim)
