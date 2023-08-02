8ba43b5636b4e0ca429b2b5e6cb43375946ddd6e
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-03 01:30:31 +0300

    feat(layer): add discrete transform version
    Add DFrFT version of the layer.

--- torch_frft/layer.py
+++ torch_frft/layer.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from torch_frft.frft import frft
+from torch_frft import dfrft, frft
 
 
 class FrFTLayer(nn.Module):
@@ -12,3 +12,13 @@ class FrFTLayer(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return frft(x, self.order, dim=self.dim)
+
+
+class DFrFTLayer(nn.Module):
+    def __init__(self, order: float = 1.0, *, dim: int = -1) -> None:
+        super().__init__()
+        self.order = nn.Parameter(torch.tensor(order, dtype=torch.float32))
+        self.dim = dim
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return dfrft(x, self.order.item(), dim=self.dim)
