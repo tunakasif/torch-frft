fix: gradient error of `frft()` for integer orders
As discussed in issue #6, the automatic gradient of the fast computation
with respect to the transformation order was problematic due to the
piecewise definition for integer orders. For example, if the order is
1.0, the transformation is only shifted FFT, which does not depend on
the order, in the sense that is automatically differentiable.

Due to 4-modulo property of the transformation, we only need to provide
differentiability to integers {-2, -1, 0, 1, 2}. Theoretical analysis
shows that for orders {-2, 0, 2}, the derivative with respect to order
at these values are 0. Hence, `a * zeros` form is appended to these
branches to keep the actual value for the transformation and provide
zero tensor for the gradient.

The remaining values {-1, 1} are supported by the `_corefrmod2()`
function since their absolute value is in the interval [0.5, 1.5]. We
used to opt to default transformation for `fft()` and `ifft()` for
orders 1 and -1, respectively. However, with this update we switch to
core calculation to provide differentiability at 1.0.

Fixes: #6

torch_frft/frft_module.py
@@ -1,5 +1,5 @@
 import torch
-from torch.fft import fft, fftshift, ifft
+from torch.fft import fft, ifft
 
 
 def ifrft(fc: torch.Tensor, a_param: torch.Tensor, *, dim: int = -1) -> torch.Tensor:
@@ -18,15 +18,11 @@ def frft(fc: torch.Tensor, a_param: torch.Tensor, *, dim: int = -1) -> torch.Ten
     elif a < -2:
         a += 4
 
-    # special integer cases
+    # special integer cases with zero gradient, hence the a * zeros
     if a == 0.0:
-        return fc
+        return fc + a * torch.zeros_like(fc, device=fc.device)
     elif a == 2.0 or a == -2.0:
-        return _dflip(fc, dim=dim)
-    elif a == 1.0:
-        return fftshift(fft(fftshift(fc), dim=dim, norm="ortho"))
-    elif a == -1.0:
-        return fftshift(ifft(fftshift(fc), dim=dim, norm="ortho"))
+        return _dflip(fc, dim=dim) + a * torch.zeros_like(fc, device=fc.device)
 
     biz = _bizinter(fc, dim=dim)
     zeros = torch.zeros_like(biz, device=fc.device).index_select(